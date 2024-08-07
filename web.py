b073b8d8431f4a4837d20d22d4bc72f37b688711
Roshan Panta <roshanpanta2059@gmail.com>
2024-08-07 16:38:46 +0545

    download stock data and lo0ad model

--- web.py
+++ web.py
@@ -6,3 +6,15 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 st.title("stock Price Predictor Web App")
+
+stock = st.text_input('Enter the stock:', "GOOG")
+
+from datetime import datetime
+end = datetime.now()
+start = datetime(end.year - 10, end.month, end.day)
+
+google_data = yf.download(stock, start, end)
+
+model = load_model('latest_stock_price_predictor_model.keras')
+st.subheader('Stock Data')
+st.write(google_data)
